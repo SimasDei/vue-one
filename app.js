4457d2135927662a4dd131f102c62277ae326de9
SimasDei <simondeiman@gmail.com>
2019-02-04 19:22:33 +0200

    implement event listeners to buttons, asign methods

---
+++ app.js
@@ -0,0 +1,29 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame: function() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      max = 12;
+      min = 5;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {}
+  }
+});
