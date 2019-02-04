cdb35eb1766cec07157df76f4986b2bff9b0a15e
SimasDei <simondeiman@gmail.com>
2019-02-04 20:00:25 +0200

    complete event handlers, implement action log

--- app.js
+++ app.js
@@ -3,27 +3,86 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      var max = 10;
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      let damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster for ' + damage
+      });
+      if (this.checkWin()) {
+        return;
+      }
 
-      max = 12;
-      min = 5;
-      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.playerHealth -= damage;
+      this.monsterAttacks();
     },
-    specialAttack: function() {},
-    heal: function() {},
-    giveUp: function() {}
+    specialAttack: function() {
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Limit Break for ' + damage
+      });
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
+      this.monsterAttacks();
+    },
+    giveUp: function() {
+      this.gameIsRunning = false;
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    monsterAttacks: function() {
+      let damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits player for ' + damage
+      });
+
+      this.checkWin();
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You are Winrar! Go Again`e ? ')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('Aw jeez :( Go Again`e ?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
   }
 });
